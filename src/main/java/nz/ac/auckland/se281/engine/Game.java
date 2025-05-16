3a9a3ebb1e927e890931328e0926322d7245b694
Mansher Bhullar <mbhu385@aucklanduni.ac.nz>
2025-05-16 15:46:37 +1200

    Task 1.1 and 1.2 simple implementation of welcome message through MessageCli and Round counter.

--- src/main/java/nz/ac/auckland/se281/engine/Game.java
+++ src/main/java/nz/ac/auckland/se281/engine/Game.java
@@ -1,15 +1,25 @@
 package nz.ac.auckland.se281.engine;
 
 import nz.ac.auckland.se281.Main.Difficulty;
+import nz.ac.auckland.se281.cli.MessageCli;
 
 public class Game {
   public static String AI_NAME = "HAL-9000";
+  private int numRounds;
 
   public Game() {}
 
-  public void newGame(Difficulty difficulty, int numRounds, String[] options) {}
+  public void newGame(Difficulty difficulty, int numRounds, String[] options) {
+    String namePlayer = options[0];
+    MessageCli.WELCOME_PLAYER.printMessage(namePlayer);
+    this.numRounds = numRounds;
+  }
 
-  public void play() {}
+  public void play() {
+    int roundCounter = 1;
+    MessageCli.START_ROUND.printMessage(roundCounter, numRounds);
+    roundCounter++;
+  }
 
   public void showStats() {}
 }
