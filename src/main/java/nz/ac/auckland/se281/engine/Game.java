28510ae823594414e09a8e6d7dfb65f430b15e88
Mansher Bhullar <mbhu385@aucklanduni.ac.nz>
2025-05-16 16:21:58 +1200

    Task 1.8, added logic for boundary cases and print move info via if statement to check valid inputs through scanner.nextLine().

--- src/main/java/nz/ac/auckland/se281/engine/Game.java
+++ src/main/java/nz/ac/auckland/se281/engine/Game.java
@@ -2,11 +2,13 @@ package nz.ac.auckland.se281.engine;
 
 import nz.ac.auckland.se281.Main.Difficulty;
 import nz.ac.auckland.se281.cli.MessageCli;
+import nz.ac.auckland.se281.cli.Utils;
 
 public class Game {
   public static String AI_NAME = "HAL-9000";
   private int numRounds;
   private int roundCounter;
+  private String namePlayer;
 
   public Game() {}
 
@@ -14,14 +16,25 @@ public class Game {
     String namePlayer = options[0];
     MessageCli.WELCOME_PLAYER.printMessage(namePlayer);
     this.numRounds = numRounds;
+    this.namePlayer = namePlayer;
     this.roundCounter = 1;
   }
 
   public void play() {
-    MessageCli.ASK_HUMAN_INPUT.printMessage();
-    MessageCli.INVALID_HUMAN_INPUT.printMessage();
     MessageCli.START_ROUND.printMessage(roundCounter, numRounds);
-    roundCounter++;
+
+    MessageCli.ASK_HUMAN_INPUT.printMessage();
+
+    String input = Utils.scanner.nextLine();
+
+    String[] parts = input.split("\\s+");
+
+    if (input == null || input.isEmpty() || parts.length != 2) {
+      MessageCli.INVALID_HUMAN_INPUT.printMessage();
+    } else {
+      roundCounter++;
+      MessageCli.PRINT_INFO_MOVE.printMessage(namePlayer, parts[0], parts[1]);
+    }
   }
 
   public void showStats() {}
